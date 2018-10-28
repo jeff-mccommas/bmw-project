f602cfa5d37c8cb117a57063a05cafd4136f8868
jeff-mccommas <jeffmccommas@gmail.com>
2018-10-28 12:04:49 -0400

    rental-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TempComponent } from './temp/temp.component';
 import { RentalComponent } from './rental/rental.component';
-
+import {Routes, RouterModule} from '@angular/router';
+import { RentalListComponent } from './rental/rental-list/rental-list.component';
+import { RentalListItemComponent } from './rental/rental-list-item/rental-list-item.component';
+const  routes: Routes = [
+  {path: '', component: RentalComponent},
+  {path: 'temp', component: TempComponent},
+]
 
 @NgModule({
   declarations: [
@@ -13,9 +19,12 @@ import { RentalComponent } from './rental/rental.component';
     HeaderComponent,
     TempComponent,
     RentalComponent,
+    RentalListComponent,
+    RentalListItemComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
